3c339345498be30ebf34c776c95171024c206fab
Naomi Porter <naomiporter.dev@gmail.com>
2021-04-29 16:08:33 +1200

    add footer text, address text, and opening hours text

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,11 @@ const App = (props) => {
   return (
     <>
       <h1>Honey's Cat Cafe</h1>
+      <div>
+        <p>Open 10am-4pm, Wednesday through Saturday.</p>
+        <p>100 Cutie Pie Lane, Wellington</p>
+      </div>
+      <footer>&copy;2021 by Lara x Naomi</footer>
     </>
   )
 }
