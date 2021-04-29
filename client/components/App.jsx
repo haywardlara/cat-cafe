172472cdbbf39a2a443e9697fce4203b3d959044
Lara Hayward <haywardlbl@gmail.com>
2021-04-29 14:35:06 +1200

    pruning unneeded files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,11 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-
-import { fetchFruits } from '../actions'
+import React from "react"
 
 const App = (props) => {
-  useEffect(() => {
-    props.dispatch(fetchFruits())
-  })
-
   return (
     <>
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {props.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
+      <p>I love cats!</p>
     </>
   )
 }
-const mapStateToProps = (globalState) => {
-  return {
-    fruits: globalState.fruits
-  }
-}
 
-export default connect(mapStateToProps)(App)
+export default App
--- client/index.js
+++ client/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-
-import reducers from './reducers'
 import App from './components/App'
+import ReactDOM from 'react-dom'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+  ReactDOM.render(
+    <App />,
     document.getElementById('app')
   )
 })
