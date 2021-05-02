57c91d8308f2eb311a2a75379a03bb9aa055763a
Lara Hayward <haywardlbl@gmail.com>
2021-05-03 10:56:56 +1200

    update site

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,15 +2,19 @@ import React from "react"
 
 const App = (props) => {
   return (
-    <div className="container">
-      <h1>Honey's Cat Cafe</h1>
-      <div><img src="cats.jpeg" alt="two cats"/></div>
-      <div>
+    <>
+      <div className="container">
+        <h1>Honey's Cat Cafe</h1>
+        <div>
+          <img src="cats.jpeg" alt="two cats" />
+        </div>
+      </div>
+      <div className="fun">
         <p>Open 10am-4pm, Wednesday through Saturday.</p>
         <p>100 Cutie Pie Lane, Wellington</p>
+        <footer>&copy;2021 by Lara x Naomi</footer>
       </div>
-      <footer>&copy;2021 by Lara x Naomi</footer>
-    </div>
+    </>
   )
 }
 
